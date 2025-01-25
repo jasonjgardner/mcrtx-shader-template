3d870cd4ceb12c9c94be0807a94728f4655762c7
Veka0 <26331365+Veka0@users.noreply.github.com>
2025-01-25 19:32:07 +0100

    Added license notice and util function

--- project/RTXStub/shaders/Include/Generated/Signature.hlsl
+++ project/RTXStub/shaders/Include/Generated/Signature.hlsl
@@ -1,3 +1,26 @@
+/* MIT License
+ * 
+ * Copyright (c) 2025 veka0
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __SIGNATURE_HLSL__
 #define __SIGNATURE_HLSL__
 
@@ -10,6 +33,12 @@
 
 // Note that float3 buffers are often stored as float4
 // so you can change their type to get an extra channel for storage.
+
+// For the most part, every single buffer here is accessible in every single pass.
+// The only exception is PreBlasSkinningCB buffer which is only accessible in PreBlasSkinning pass
+// and that same pass only has access to PreBlasSkinning and a single descriptor table
+// with the following buffers: indexBuffers, vertexBuffers, faceDataBuffers, faceUvBuffers, 
+// vertexIrradianceCache, faceIrradianceCache, faceDataBuffersRW, faceUvBuffersRW, vertexBuffersRW
 
 // 32BIT_CONSTANTS
 cbuffer RootConstants : register(b2) {
--- project/RTXStub/shaders/Include/Util.hlsl
+++ project/RTXStub/shaders/Include/Util.hlsl
@@ -1,7 +1,38 @@
+/* MIT License
+ * 
+ * Copyright (c) 2025 veka0
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef __UTIL_HLSL__
 #define __UTIL_HLSL__
 
 #include "Generated/Signature.hlsl"
+
+uint3 getDispatchDimensions() {
+    return uint3(
+        (g_dispatchDimensions >> 0*10) & 1023, 
+        (g_dispatchDimensions >> 1*10) & 1023,
+        g_dispatchDimensions >> 2*10
+    );
+}
 
 float2 computeMotionVector(float3 steveSpacePositon, float3 steveSpaceMotion) {
     float4 clipPos = mul(float4(steveSpacePositon, 1), g_view.viewProj);
